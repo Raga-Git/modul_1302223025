9fc40aae857052b216c39e0757dd65bb4ffa359d
unknown <ragadhitya@gmail.com>
2024-03-13 12:52:37 +0700

    Menambahkan class KodeBuah

--- modul4_1302223025/Program.cs
+++ modul4_1302223025/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static Program.KodeBuah;
+public class Program
+{
+    public class KodeBuah
+    {
+        public enum namaBuah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        }
+
+        public static string getKodeBuah(namaBuah nama)
+        {
+            string[] kodeBuah = { "A00","B00","C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
+            return kodeBuah[(int)nama]; 
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Buah " + namaBuah.Kurma + " Memiliki kode buah " + getKodeBuah(namaBuah.Kurma));
+    }
+}
--- modul4_1302223025/obj/Debug/net8.0/modul4_1302223025.AssemblyInfo.cs
+++ modul4_1302223025/obj/Debug/net8.0/modul4_1302223025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul4_1302223025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e1ac247e9b257e120997793a1fcfc7d2c7dc85")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul4_1302223025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul4_1302223025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
